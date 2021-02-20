823cd1746af7ec2f8b92706f02fdb16017537776
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-02-20 18:35:50 +0100

    Added error handlers for common errors

--- app.py
+++ app.py
@@ -210,8 +210,49 @@ def create_app(test_config=None):
         province = body.get('province')
         district.province = province if province else district.province
 
-    @app.errorhandler(400)
+    @app.errorhandler(422)
+    def unprocessable(error):
+    return jsonify({
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
 
+
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+    return jsonify({
+        "success": False, 
+        "error": 401,
+        "message": "Unauthorized"
+        }), 401
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return jsonify({
+        "success": False, 
+        "error": 403,
+        "message": error.message
+        }), 403
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "not found"
+        }), 404
+        
+
+    '''   
+    @app.errorhandler(AuthError)
+    def handle_auth_error(exception):
+
+    response = jsonify(exception.error)
+    response.status_code = exception.status_code
+    return response
+    '''
 
 
 
